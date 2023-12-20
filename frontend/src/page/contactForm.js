0ce59333935fccc9d5c9e42d5ab134fe9ef380a5
suganthidhanasekaran <ssuganthi@techtist.com>
2023-12-20 20:42:46 +0530

    contact form

--- frontend/src/page/contactForm.js
+++ frontend/src/page/contactForm.js
@@ -52,62 +52,7 @@ const ContactForm = ({ handleTryOff }) => {
                         `}
                         >
                             Contact Details
-                        </h1>
-                        {/* <div className="max-w-3xl  rounded-2xl border-5 border-[#e78b08] p-4  bg-black-rgba">
-                            <div className="mx-auto w-full  flex flex-col space-y-2">
-                                <div className="flex py-1">
-                                    <input
-                                        className={`w-full px-2 py-2  rounded-lg font-medium border-2 border-transparent placeholder-gray-500 
-                                text-sm focus:outline-none  focus:border-2  focus:outline bg-gray-100 text-gray-600 focus:border-white"`}
-                                        type="text"
-                                        placeholder="Enter your Name"
-                                    />
-
-                                </div>   
-
-                                <div className="flex py-1">
-
-                                    <input
-                                        className={`w-full px-2 py-2 rounded-lg  font-medium border-2 border-transparent placeholder-gray-500 text-sm focus:outline-none focus:border-2  focus:outline ${darkMode
-                                            ? "bg-[#302E30] text-white focus:border-white"
-                                            : "bg-gray-100 text-black focus:border-black"
-                                            }`}
-                                        type="tel"
-                                        placeholder="Phone Number"
-                                    />
-                                </div>
-                                <div className="flex py-1">
-                                    <input
-                                        className={`w-full px-2 py-2 rounded-lg  font-medium border-2 border-transparent placeholder-gray-500 text-sm focus:outline-none focus:border-2  focus:outline ${darkMode
-                                            ? "bg-[#302E30] text-white focus:border-white"
-                                            : "bg-gray-100 text-black focus:border-black"
-                                            }`}
-                                        type="tel"
-                                        placeholder="Email Id"
-                                    />
-                                </div>
-                                <div className="flex py-1">
-                                    <textarea
-                                        className={`w-full px-2 py-2 rounded-lg  font-medium border-2 border-transparent placeholder-gray-500 text-sm focus:outline-none focus:border-2  focus:outline ${darkMode
-                                            ? "bg-[#302E30] text-white focus:border-white"
-                                            : "bg-gray-100 text-black focus:border-black"
-                                            }`}
-                                        type="Enquiry"
-                                        placeholder="Enquiry"
-                                    />
-                                </div>
-
-                                <div className="flex mt-3 py-2 justify-center">
-                                    <a href
-                                        className="w-[200px] p-2 font-semibold border-4  border-[#e78b08] text-[20px]  text-deep-purple-900  rounded-xl bg-[#dfcb18] text-center
-                               cursor-pointer no-underline focus:shadow-outline focus:outline-none"
-                                        onClick={() => handleSubmit()}
-                                    >
-                                        Submit
-                                    </a>
-                                </div>
-                            </div>
-                        </div> */}
+                        </h1>  
 
                         {/* Change Design */}
                         <div className='flex justify-center basis-1/2'>
