77c30ab6c79ae3b5bfa22a02d35d620328e500d1
suganthidhanasekaran <ssuganthi@techtist.com>
2024-01-08 22:07:11 +0530

    added db config username pwd

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,13 +1,28 @@
+// console.log('db connect')
+// module.exports = {
+//     HOST : "localhost",
+//     USER : "root",
+//     DB : 'skie',
+//     dialect :  "mysql",
+//     pool : {
+//         max : 5,
+//         min : 0,
+//         acquire : 30000,
+//         idle : 10000
+//     }
+// }
+
 console.log('db connect')
 module.exports = {
-    HOST : "localhost",
-    USER : "root",
-    DB : 'skie',
-    dialect :  "mysql",
-    pool : {
-        max : 5,
-        min : 0,
-        acquire : 30000,
-        idle : 10000
+    HOST: "localhost",
+    USER: "magicdb1",
+    DB: 'magicseeds',
+    PASSWORD: 'cAr*bLacK@75',
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 }
