46367cb784c23f556d5f742f36952f1a2cc71b89
suganthidhanasekaran <ssuganthi@techtist.com>
2024-01-10 11:30:50 +0530

    added axios ipaddress

--- frontend/src/page/gift1Form.js
+++ frontend/src/page/gift1Form.js
@@ -10,7 +10,7 @@ const GetUserForm = ({ handleTryOff }) => {
     let textContent = "To avail for Gold Coin, kindly fill and submit"
 
      useEffect(() => {
-        const url = "http://localhost:8080/api/getAllCampaignType";
+        const url = "http://13.232.110.68:8080/api/getAllCampaignType";
         axios.get(url).then(response => {           
             console.log("Full Data : ", response.data)
         })
