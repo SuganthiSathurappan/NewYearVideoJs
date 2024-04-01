e036cb7163a58f4f73cfe688a2b2139251e2326e
suganthidhanasekaran <ssuganthi@techtist.com>
2024-04-01 18:15:46 +0530

    IOS landscape rotation added

--- frontend/src/HdfcVideoJs/mainVideoJs.js
+++ frontend/src/HdfcVideoJs/mainVideoJs.js
@@ -139,14 +139,18 @@ const Video = () => {
         player.current.controlBar.removeChild('VolumePanel');
         // player.current.addClass("hide-controls");
         // // configure plugins
-        // player.current.landscapeFullscreen({
-        //   fullscreen: {
-        //     enterOnRotate: true,
-        //     exitOnRotate: true,
-        //     alwaysInLandscapeMode: true,
-        //     iOS: true
-        //   }
-        // })
+        player.current.landscapeFullscreen({
+          fullscreen: {
+            enterOnRotate: true,
+            exitOnRotate: true,
+            alwaysInLandscapeMode: true,
+            iOS: {
+              enterOnRotate: true,
+              exitOnRotate: true,
+              forceLandscape: true
+            }
+          }
+        });
         player.current.on("ended", () => {
           console.log("ended");
         });
